8f0aac72159bf38ab186c8c2a0a8529782434b03
AppliedGit <62598762+AppliedGit@users.noreply.github.com>
2022-07-12 10:24:08 +0530

    Update settings.py

--- Restaurant_CLV/settings.py
+++ Restaurant_CLV/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-d9qhqi+ieg^_i9-=6v^1$fs!snv6+si38!i$n!13wbss&rm!30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-#ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
